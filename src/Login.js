98466bd3b2d1d3c937096b03013da5b8db597e58
tsepomatlaletsa-cyber <bolotinmatlaletsa@gmail.com>
2025-09-28 13:23:12 -1200

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -10,7 +10,7 @@ function Login({ onLogin }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/login", { email, password });
+      const res = await axios.post("https://system-backend-2-ty55.onrender.com/login", { email, password });
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("role", res.data.role);
       onLogin(res.data.role);
